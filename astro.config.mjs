3504e590d3f641790e6510350f7037764c8d5396
Emre Can Çorapçı <emrecancorapci@gmail.com>
2026-01-26 23:56:37 +0300

    feat: Add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mrcn.tr',
   vite: {
     plugins: [tailwindcss()]
   },
+  integrations: [sitemap()],
 });
