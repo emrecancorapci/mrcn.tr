ea7b9cec4c47bf9674a6359236618285874c70e2
Emre Can Çorapçı <emrecancorapci@gmail.com>
2026-03-02 01:21:59 +0300

    feat: update astro config to change color theme of the code snippets in markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 // @ts-check
 
-import sitemap from '@astrojs/sitemap';
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mrcn.tr',
+  site: "https://mrcn.tr",
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
   integrations: [sitemap()],
+  markdown: {
+    shikiConfig: {
+      theme: "everforest-dark",
+      wrap: true,
+    },
+  },
 });
