bdbbac5d7bd7012bc0b962bdbb2c19e59d32c520
Emre Can Çorapçı <emrecancorapci@gmail.com>
2026-03-02 00:07:07 +0300

    feat: separate project block component and add support for placement above summary

--- src/schemas/project.schema.ts
+++ src/schemas/project.schema.ts
@@ -2,20 +2,30 @@ import { z } from "astro/zod";
 
 const blockTags = z.object({
   title: z.string(),
+  onTop: z.boolean().optional(),
   type: z.literal("tags"),
   content: z.array(z.string()),
 });
 
 const blockPlain = z.object({
   title: z.string(),
+  onTop: z.boolean().optional(),
   type: z.literal("plain"),
   content: z.string(),
 });
 
 const blockRecords = z.object({
   title: z.string(),
+  onTop: z.boolean().optional(),
   type: z.literal("records"),
   content: z.array(z.tuple([z.string(), z.string()])),
+});
+
+const blockList = z.object({
+  title: z.string(),
+  onTop: z.boolean().optional(),
+  type: z.literal("list"),
+  content: z.array(z.string()),
 });
 
 const link = z.object({
@@ -38,5 +48,8 @@ export const projectSchema = z.object({
   mainTags: z.array(z.string()),
   tags: z.array(z.string()).optional(),
   links: z.array(link).optional(),
-  blocks: z.array(z.union([blockTags, blockPlain, blockRecords])).optional(),
+  blocks: z.array(z.union([blockTags, blockPlain, blockRecords, blockList])).optional(),
 });
+
+export type Project = z.infer<typeof projectSchema>;
+export type ProjectBlock = z.infer<typeof blockTags | typeof blockPlain | typeof blockRecords | typeof blockList>;
