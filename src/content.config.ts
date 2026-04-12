99e5cef6587817cc478aeca5ebc75f31efd98f7a
Emre Can Çorapçı <emrecancorapci@gmail.com>
2026-04-12 18:55:37 +0300

    feat: add skills data and schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,6 +2,7 @@ import { defineCollection } from "astro:content";
 import { file, glob } from "astro/loaders";
 
 import { projectSchema, projectTypeSchema } from "./schemas/project.schema";
+import { skillsSchema } from "./schemas/skills.schema";
 
 const project = defineCollection({
   loader: glob({ pattern: "*.md", base: "./src/data/projects" }),
@@ -13,4 +14,9 @@ const projectType = defineCollection({
   schema: projectTypeSchema,
 });
 
-export const collections = { project, projectType };
+const skills = defineCollection({
+  loader: file("./src/data/skills.json"),
+  schema: skillsSchema,
+});
+
+export const collections = { project, projectType, skills };
